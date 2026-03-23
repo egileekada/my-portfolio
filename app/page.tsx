66ebda40f61345cd29faf7e7537c2096e4492ccb
egileekada <egileoniso.ekada@gmail.com>
2026-03-24 00:01:51 +0100

    chore: update favicon and clean up imports in page component

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
-import Image from "next/image";
+import Skills from "@/components/Skills"; 
 
 export default function Home() {
   return (
